51b942bfb519675fe9db760d5c4cd6d092828b6a
spiderxm <mrigank.rkee@rediffmail.com>
2020-03-19 21:45:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+const passport = require('passport');
+
+const session = require('express-session');
+
+const db = require('./config/keys').MongoUrl;
+
+app.use(expressLayouts);
+console.log(db);
+
+app.set('view engine', 'ejs');
+
+const mongoose = require('mongoose');
+
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Mongo Db connected"))
+    .catch(err => console.log(err));
+
+app.use('/', require('./routes/index'));
+
+app.use('/users', require('./routes/users'));
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, console.log(`Server started on Port ${Port}`))
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoUrl: 'mongodb+srv://login:login@cluster0-9pg2w.mongodb.net/test?retryWrites=true&w=majority'
+}
