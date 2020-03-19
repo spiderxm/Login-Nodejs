df8b40bdd69fd9c8d405ae1aa89a0bffd45780ca
spiderxm <mrigank.rkee@rediffmail.com>
2020-03-20 03:15:14 +0530

    final commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
+const { ensureAuthenticated } = require('../config/auth');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render("welcome"));
+
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
+    name: req.user.name
+}));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -87,4 +87,10 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+})
+
 module.exports = router;
